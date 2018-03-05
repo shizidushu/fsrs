8d6a1b4c15d1a6f84a43238596bf612f3e3ae09f
shizidushu <shizidushu@gmail.com>
2018-03-06 01:33:57 +0800

    2

--- R/trigonometric.R
+++ R/trigonometric.R
@@ -1,5 +1,7 @@
 # Reference to 369  Thomas Calculus Early Transcendentals 13th
 
+#### Angles --------------------------------------------------------####
+
 #' The length of an arc
 #' 
 #' @param radius The radius of a circle
@@ -31,4 +33,61 @@ to_degree <- function(theta) {
 to_radian <- function(theta) {
   degree_1_in_theta <- pi / 180
   degree_1_in_theta * theta
+}
+
+
+
+
+#### The Six Basic Trigonometric Functions ------------------------####
+
+#' sine
+#' 
+#' @param y y coordinate in a circle or opposite of an acute angle in terms of a right triangle
+#' @param r radius or hypotenuse of an acute angle in terms of a right triangle
+#' @export
+sine <- function(y, r) {
+  y / r
+}
+
+#' cosecant
+#' 
+#' @inherit  sine
+#' @export
+cosecant <- function(r, y) {
+  r / y
+}
+
+
+#' cosine
+#' 
+#' @param x x coordinate in a circle or adjacent of an acute angle in terms of a right triangle
+#' @inheritParams sine 
+#' @export
+cosine <- function(x, r) {
+  x / r
+}
+
+#' secant
+#' 
+#' @inherit cosine 
+#' @export
+secant <- function(r, x) {
+  r / x
+}
+
+#' tangent
+#' 
+#' @inherit sine
+#' @inherit cosine
+#' @export
+tangent <- function(y, x) {
+  y / x
+}
+
+#' cotangent
+#' 
+#' @inherit tangent
+#' @export
+cotangent <- function(x, y) {
+  x / y
 }
